c7a3318a2d05024c824c11d18e076ba1a39a2e49
Bertrand Cachet <bertrand.cachet@exoscale.ch>
2025-12-20 15:46:39 +0100

    Terraform instance & ensure systemd-resolved does not start DNS

---
+++ README.md
@@ -0,0 +1,40 @@
+# Home Sweet Home
+
+Objective is to create a server with following services:
+- Ad Blocker
+  - [AdGuard Home](https://adguard.com/en/adguard-home/overview.html)
+  - [Pi-Hole]()
+- Filter inappropriate web content
+  - [AdGuard Home](https://adguard.com/en/adguard-home/overview.html)
+- Password Manager
+  - [VaultWarden](https://github.com/dani-garcia/vaultwarden)
+  - [BitWarden Lite](https://github.com/bitwarden/self-host/tree/main/bitwarden-lite)
+
+I plan to deploy each services via containers.
+I can rely on a managed Kubernetes solution like [Exoscale SKS offering](https://www.exoscale.com/sks/).
+I would easily find Helm charts (official or not) for the various services I plan to use.
+
+But I want to use this opportunity to learn things.
+In the initial implementation, I plan to rely on the following:
+- Podman Quadlet to run workloads in Podman containers as systemd services
+- Traefik as reverse DNS
+
+In a second stage, I would like to implement zero downtime on top of this approach via one of the following approach
+- [Podman socket activation](https://github.com/containers/podman/blob/main/docs/tutorials/socket_activation.md) + 
+  [systemd-socket-proxyd](https://www.man7.org/linux/man-pages/man8/systemd-socket-proxyd.8.html) for services 
+  that do not support socket activation natively.
+
+  Inspiration:
+  - https://www.redhat.com/en/blog/painless-services-implementing-serverless-rootless-podman-and-systemd
+  - https://thinkaboutit.tech/posts/2025-07-20-adhoc-containers-with-systemd-and-quadlet/
+- Blue/Green deployment relying on Traefik & scripts to stop _old_ deployment
+
+  Inspiration:
+  - https://github.com/evolutics/zero-downtime-deployments-with-podman-docker-or-docker-compose
+  - https://github.com/dryaf/deploy
+
+I plan to define workload with Cuelang.
+I will need to define Cuelang structure that represent Podman Quadlet Container:
+- [Podman Quadlet Container definition](https://docs.podman.io/en/latest/markdown/podman-systemd.unit.5.html)
+
+
---
+++ terraform/instance.tf
@@ -0,0 +1,89 @@
+data "exoscale_template" "template" {
+  zone = var.zone
+  name = var.template_name
+}
+
+data "ct_config" "ollama" {
+  strict       = true
+  pretty_print = true
+  snippets     = []
+  content = yamlencode(
+    {
+      variant = "fcos"
+      version = "1.5.0"
+      passwd = {
+        users = [
+          {
+            name                = "core"
+            ssh_authorized_keys = [var.ssh_key.public_key]
+          }
+        ]
+      }
+      storage = {
+        files = [
+          {
+            path = "/etc/sysctl.d/20-dns-privileged-port.conf"
+            contents = {
+              inline = <<-EOT
+              net.ipv4.ip_unprivileged_port_start=53
+              EOT
+            }
+          },
+          {
+            path = "/etc/systemd/resolved.conf.d/adguard-dns.conf"
+            contents = {
+              inline = <<-EOT
+              [Resolve]
+              DNS=127.0.0.1
+              DNSStubListener=no
+              EOT
+            }
+          }
+        ]
+      }
+    }
+  )
+}
+
+resource "exoscale_compute_instance" "hsh" {
+  name               = "hsh"
+  zone               = var.zone
+  type               = "standard.medium"
+  disk_size          = 100
+  template_id        = data.exoscale_template.template.id
+  security_group_ids = [exoscale_security_group.hsh.id]
+  ssh_keys           = [var.ssh_key.name]
+
+  user_data = data.ct_config.ollama.rendered
+}
+
+resource "exoscale_security_group" "hsh" {
+  name = "hsh"
+}
+
+resource "exoscale_security_group_rule" "hsh_ssh" {
+  security_group_id = exoscale_security_group.hsh.id
+  type              = "INGRESS"
+  protocol          = "TCP"
+  cidr              = "0.0.0.0/0"
+  start_port        = 22
+  end_port          = 22
+}
+
+resource "exoscale_security_group_rule" "hsh_dns_tcp" {
+  security_group_id = exoscale_security_group.hsh.id
+  type              = "INGRESS"
+  protocol          = "TCP"
+  cidr              = "0.0.0.0/0"
+  start_port        = 53
+  end_port          = 53
+}
+
+resource "exoscale_security_group_rule" "hsh_dns_udp" {
+  security_group_id = exoscale_security_group.hsh.id
+  type              = "INGRESS"
+  protocol          = "UDP"
+  cidr              = "0.0.0.0/0"
+  start_port        = 53
+  end_port          = 53
+}
